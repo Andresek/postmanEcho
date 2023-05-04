15229aa2ff5aac324da495ab8aa908d8ad29aece
Andrew S <samum_94@mail.ru>
2023-05-04 07:02:40 +0300

    Delete all not used tests

--- src/test/java/ru/netology/rest/TestPostmanApi.java
+++ src/test/java/ru/netology/rest/TestPostmanApi.java
@@ -6,20 +6,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.stringContainsInOrder;
 
-class MobileBankApiTestV1 {
-    @Test
-    void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
-                .when()
-                .get("/demo/accounts")
-                // Проверки
-                .then()
-                .statusCode(200);
-    }
+class TestPostmanApi {
 
     @Test
     void demoPost() {
